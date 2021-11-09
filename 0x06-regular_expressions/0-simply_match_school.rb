27a32993e8308c0c0097ca6c99984e515657e087
Alejandro Alomia <alejandroalomia730@gmail.com>
2021-11-08 22:20:07 -0500

    add file

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/ruby
-expresion = puts ARGV[0].scan(/School/).join
+puts ARGV[0].scan(/School/).join
--- READMED.md
+++
@@ -1,1 +0,0 @@
-This repository contains some scripts that I am learning at Holberton School.
