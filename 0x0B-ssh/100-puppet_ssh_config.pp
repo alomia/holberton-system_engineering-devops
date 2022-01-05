ec646d4e062ccd4c0dc529e95c71faa8fb2cfe91
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-01-04 23:21:42 -0500

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#Client configuration file (w/ Puppet)
+
+file_line { 'file':
+  path    => '/etc/ssh/ssh_config',
+  ensure => 'present',
+  line    => '	IdentityFile ~/.ssh/school',
+}
+
+file_line { 'skip password':
+  path    => '/etc/ssh/ssh_config',
+  ensure => 'present',  
+  line    => '	PasswordAuthentication no',
+}
