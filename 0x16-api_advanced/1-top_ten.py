88ca5c6cc36a72df75ce25285a5983402380e553
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-15 22:49:26 -0500

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""import requests"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the 10 hottest posts on a given subreddit"""
+    limit = 10
+    try:
+        url = 'https://www.reddit.com/r/{subreddit}/hot.json?limit={l}'.format(
+            subreddit=subreddit, l=limit)
+        request = requests.get(
+            url, headers={'User-agent': 'MyHolbertonAPI/0.0.1'})
+        r = request.json()['data']
+        [print(value['data']['title']) for value in r['children']]
+    except(Exception):
+        print('None')
