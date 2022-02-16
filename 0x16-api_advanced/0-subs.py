31a17722cdd8339644a49762a003a3afeb95de42
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-15 22:38:46 -0500

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,11 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of subscribers  for a given subreddit"""
     try:
-        url = 'https://www.reddit.com/r/{subreddit}/about.json'.format(subreddit = subreddit)
-        response = requests.get(url, headers={'User-agent':
-                                'MyHOlbertonAPI/0.0.1'})
-        data = response.json()['data']['subscribers']
-        return data
+        url = 'https://www.reddit.com/r/{subreddit}/about.json'.format(
+            subreddit=subreddit)
+        request = requests.get(
+            url, headers={'User-agent': 'MyHolbertonAPI/0.0.1'})
+        r = request.json()['data']['subscribers']
+        return r
     except(Exception):
-        return 0
+        return(0)
