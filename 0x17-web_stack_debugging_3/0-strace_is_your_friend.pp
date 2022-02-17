afe25919d43cecf77e90323d25a40ecd3c5f4251
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-02-16 23:06:23 -0500

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Puppet bug fixed 
+# Fiz error 500 Internal server error
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
+
